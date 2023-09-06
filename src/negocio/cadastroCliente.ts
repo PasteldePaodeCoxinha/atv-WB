2bb6054a884e0561f6aef142602009f50bdd52d5
Pastel Caique <caiquepericles.23@gmail.com>
2023-09-06 10:54:13 -0300

    Adição de genero na listagemCliente

--- src/negocio/cadastroCliente.ts
+++ src/negocio/cadastroCliente.ts
@@ -17,7 +17,7 @@ export default class CadastroCliente extends Cadastro {
         let nomeSocial = this.entrada.receberTexto(`Por favor informe o nome social do cliente: `)
         let genero = this.entrada.receberTexto(`Por favor informe o seu gênero (masculino ou feminino): `)
         let valor = this.entrada.receberTexto(`Por favor informe o número do cpf: `);
-        let data = this.entrada.receberTexto(`Por favor informe a data de emissão do cpf, no padrão dd/mm/yyyy: `);
+        let data = this.entrada.receberTexto(`Por favor informe a data de emissão do cpf (dd/mm/yyyy): `);
         let partesData = data.split('/')
         let ano = new Number(partesData[2].valueOf()).valueOf()
         let mes = new Number(partesData[1].valueOf()).valueOf()
